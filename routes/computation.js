5df7e01e4a9d02867fa3d643521f8a38d501fc05
ChinthakayalaShruthi <shruthichinthakayala@gmail.com>
2022-10-26 23:15:15 -0500

    updated computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -10,13 +10,13 @@ var val1="Math.atan(x) applied to "+ x + " is "+Math.cos(x)
 
 var val2="Math.exp() applied to "+ x +"and"+ y +" is "+ Math.asin(x) 
 
-var val3= "Math.expm1() applied to "+ x + " is "+ Math.asinh();
+var val3= "Math.expm1() applied to "+ x + " is "+ Math.asinh(x);
 
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
 
-  res.render('computation', { title:'Shruthi Chinthakayala ',value1: val1, value2:vasl2, value3:val3 });
+  res.render('computation', { title:'Shruthi Chinthakayala ',value1: val1, value2:val2, value3:val3 });
 
 });
 
